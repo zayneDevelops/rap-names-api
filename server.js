a48f7408aeece84a9e4bd1191889087bb0796725
zayneDevelops <126347157+zayneDevelops@users.noreply.github.com>
2023-12-27 00:19:42 -0600

    working rapper api

---
+++ server.js
@@ -0,0 +1,38 @@
+const express  = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England',
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'birthLocation': 'Chicago, Illinois',       
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birtLocation': 'unknown'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html') //dirname is server directory/root
+})
+
+app.get('/api/:name', (request, response) =>{
+    const rapperName = request.params.name.toLowerCase()
+    if (rappers[rapperName]){ //if rapper's name is in the rapper object
+        response.json(rappers[rapperName])
+    } else {
+        response.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}.`)
+})
